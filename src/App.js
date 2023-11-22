eb21ce7d1eda2da2991a4ca224ab3f9facaedd5f
Austin <austin.x.diesta@kohls.com>
2023-11-22 12:14:55 -0600

    added some UI elements, removed boiler css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='AppContainer'>
+      <form className='note-form'>
+        <input placeholder='Title' required />
+          <textarea placeholder='Content' rows={10} required />
+          <button type='submit'>Add Note</button>
+      </form>
+      <div className='notes-grid'>
+        <div className='notes-item'>
+          <div className='notes-header'>
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+      </div>
     </div>
   );
 }

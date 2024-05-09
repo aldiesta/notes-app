5200732e58fe1cd3b39e958dfe3376aa63045fd1
Austin <austin.x.diesta@kohls.com>
2024-05-09 14:51:25 -0500

    react app, added talwind, then added kds tailwind

---
+++ Documents/dev/kds-tailwind-playground/kds-tailwind-playground-test/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button
+        type="submit"
+        className="kds-btn-primary"
+      >
+        Continue
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ Documents/dev/kds-tailwind-playground/kds-tailwind-playground-test/tailwind.config.js
@@ -0,0 +1,9 @@
+import kdsTheme from '@kohls/kds-tailwind'
+
+const shopTailwindConfig = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: kdsTheme,
+}
+export default shopTailwindConfig
